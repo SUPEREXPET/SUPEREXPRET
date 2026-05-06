219dc05c0524a52d17a32fdeafa00860976cbf05
SUPEREXPET <raniroy12143@gmail.com>
2026-05-06 19:56:01 +0530

    Update index (2).js

--- index (2).js
+++ index (2).js
@@ -35,7 +35,7 @@ bot.on("channel_post", async (msg) => {
         action: "add",
         service: "1023",
         link: postLink,
-        quantity: "1400"
+        quantity: "1200"
       });
 
       console.log("✅ 𝓞𝓻𝓭𝓮𝓻 𝓢𝓾𝓬𝓬𝓮𝓼𝓼𝓯𝓾𝓵𝓵");
